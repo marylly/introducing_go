91e357d00dbc31192156e2e9f68f51e80161b637
Marylly Araujo Silva <marylly.silva@agilitynetworks.com.br>
2018-07-24 14:02:57 -0300

    Include Interfaces topic, chapter 7

---
+++ chapter7/Interfaces/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	x float64
+	y float64
+	r float64
+}
+
+func (c *Circle) area() float64{
+	return math.Pi * c.r*c.r
+}
+
+type Shape interface {
+	area() float64
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64{
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+func distance(x1, y1, x2, y2 float64) float64{
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func totalArea(shapes ...Shape) float64 {
+	var total float64
+	for _, s := range shapes {
+		total += s.area()
+	}
+	return total
+}
+
+func main() {
+	c := Circle{0, 0, 5}
+	r := Rectangle{0, 0, 10, 10}
+
+	fmt.Println(totalArea(&c, &r))
+}
