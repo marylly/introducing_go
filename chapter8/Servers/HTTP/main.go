d50e2b644de040e15a673fcf6df5d319b899e9d3
Marylly Araújo Silva <mymarylly@gmail.com>
2018-08-07 16:00:11 -0300

    Include HTTP server topic, chapter 8

---
+++ chapter8/Servers/HTTP/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+
+	io.WriteString(
+		res,
+		`<DOCTYPE html>
+		<html>
+			<head>
+				<title>Hello, World</title>
+			</head>
+			<body>
+				Hello, World
+			</body>
+		</html>`,
+	)
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":9000", nil)
+}
