9206e990ff7872b10607548db072274cdca2b7f0
Marylly Araújo Silva <mymarylly@gmail.com>
2018-08-08 20:26:18 -0300

    Include Parsing Command-Line Arguments topic, chapter 8

---
+++ chapter8/Servers/ParsingCommand-LineArguments/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"math/rand"
+)
+
+func main() {
+	// Define flags
+	maxp := flag.Int("max", 6, "the max value")
+
+	// Parse
+	flag.Parse()
+
+	// Generate a number between 0 and max
+	fmt.Println(rand.Intn(*maxp))
+}
