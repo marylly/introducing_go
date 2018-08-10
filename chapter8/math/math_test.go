6df455d760bf76fd6787c05dd1427f5f36c87b7f
Marylly Araújo Silva <mymarylly@gmail.com>
2018-08-09 21:33:35 -0300

    Improve math package tests, chapter 9

--- chapter8/math/math_test.go
+++ chapter8/math/math_test.go
@@ -2,9 +2,26 @@ package math
 
 import "testing"
 
+type testpair struct {
+	values []float64
+	average float64
+}
+
+var tests = []testpair{
+	{ []float64{1, 2}, 1.5},
+	{ []float64{1, 1, 1, 1, 1, 1}, 1},
+	{ []float64{-1, 1}, 0},
+}
+
 func TestAverage(t *testing.T) {
-	v := Average([]float64{1, 2})
-	if v != 1.5 {
-		t.Error("Expected 1.5, got ", v)
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
 	}
 }
